3485dc5d807455c2470a5d48dd0d626bf8ac86b6
Willian <wildom741@gmail.com>
2024-09-06 17:17:06 -0300

    primeira versao

---
+++ app.js
@@ -0,0 +1,50 @@
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    // criando pesquisa expecifica
+    let resPesquisa = document.getElementById("Res-pesquisa").value;
+
+    //Se o campo estiver vazio retorna a nada encontrado
+    if (!resPesquisa){
+        //escrevendo um comentario caso a saida esja essa
+        section.innerHTML = "<p>Entrada invalida ou não existente</p>"
+        return
+    }
+
+    resPesquisa = resPesquisa.toLowerCase();
+
+
+    // Inicializa uma string vazia para armazenar os resultados
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
+    // Itera sobre cada dado da lista de dados
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+        tags = dado.tags.toLowerCase();
+        if(titulo.includes(resPesquisa) || descricao.includes(resPesquisa) || tags.includes(resPesquisa)){
+            // Cria um novo elemento HTML para cada resultado
+            resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href=${dado.link} target="_blank">Mais informações</a>
+            </div>`;
+    }
+
+    if (!resultados){
+        resultados = "<p>Nem uma resposta encontrada</p>"
+    }
+
+    }
+
+
+    // Atribui os resultados gerados à seção HTML
+    section.innerHTML = resultados;
+}
---
+++ dados.js
@@ -0,0 +1,50 @@
+let dados = [
+    {
+        titulo: "Albert Einstein",
+        descricao: "Na infância, Einstein tinha dificuldades de fala e era considerado lento por seus professores. Ele só começou a falar fluentemente depois dos quatro anos de idade e teve dificuldades na escola.",
+        link: "hhttps://pt.wikipedia.org/wiki/Albert_Einstein",
+        tags: "infância infancia aisten começou comecou homen masculino"
+    }, 
+    {
+        titulo: "Thomas Edison",
+        descricao: "Considerado um estudante 'lento' na escola, Edison foi educado em casa por sua mãe após ser retirado da escola. Ele tinha dificuldades auditivas e acreditava-se que tinha algum tipo de distúrbio de aprendizagem.",
+        link: "https://pt.wikipedia.org/wiki/Thomas_Edison",
+        tags: "após acreditava acreditava-se distúrbio disturbio homen masculino"
+    },
+    {
+        titulo: "Walt Disney",
+        descricao: "Disney enfrentou dificuldades com a escola e foi diagnosticado posteriormente com dislexia. Ele foi demitido várias vezes e teve falhas antes de fundar a Disney.",
+        link: "https://pt.wikipedia.org/wiki/Walt_Disney",
+        tags: "dislexia homen masculino"
+    },
+    {
+        titulo: "Frederick Douglass",
+        descricao: "Douglass nasceu escravo e enfrentou extrema violência e condições de vida desumanas. Ele aprendeu a ler e escrever em segredo e mais tarde se tornou um importante abolicionista e defensor dos direitos civis.",
+        link: "https://pt.wikipedia.org/wiki/Frederick_Douglass",
+        tags: "violência violencia condições condicoes abolir homen masculino"
+    },
+    {
+        titulo: "Charlie Chaplin",
+        descricao: "Chaplin cresceu na pobreza extrema em Londres, vivendo em asilos para os pobres. Ele perdeu o pai cedo e sua mãe foi internada em um sanatório. Chaplin se tornou um dos maiores ícones do cinema.",
+        link: "https://pt.wikipedia.org/wiki/Charlie_Chaplin",
+        tags: "mãe mae sanatório sanatorio ícones ícone icones icone homen masculino"
+    },
+    {
+        titulo: "Stephen Hawking",
+        descricao: "Embora ele tenha tido uma infância e adolescência relativamente normais, Hawking foi diagnosticado com esclerose lateral amiotrófica (ELA) na universidade, uma doença que o paralisou. Mesmo assim, ele revolucionou o campo da física.",
+        link: "https://pt.wikipedia.org/wiki/Stephen_Hawking",
+        tags: "infância infancia diagnosticado diagnostico amiotrófica amiotrofica doença doenca paralisia física físico fisica fisico homen masculino"
+    },
+    {
+        titulo: "Helen Keller",
+        descricao: "Keller perdeu a visão e a audição quando tinha 19 meses de idade devido a uma doença. Com a ajuda de sua professora Anne Sullivan, ela aprendeu a se comunicar e tornou-se uma ativista famosa pelos direitos das pessoas com deficiência.",
+        link: "https://pt.wikipedia.org/wiki/Helen_Keller",
+        tags: "visão audição visao audicao ativista deficiência deficiencia mulher feminino"
+    },
+    {
+        titulo: "Vincent van Gogh",
+        descricao: "Van Gogh sofria de depressão e vários problemas de saúde mental. Ele só começou a pintar aos 27 anos e morreu sem reconhecimento, mas é hoje considerado um dos maiores artistas da história.",
+        link: "https://pt.wikipedia.org/wiki/Vincent_van_Gogh",
+        tags: "depressão depressao saúde saude artistas artista homen masculino"
+    }
+];
